07586083103181f621692656f924949a81b93276
shitalVishwakarma <shitalvishwakarma07@gmail.com>
2025-04-28 19:40:57 +0530

    It's a commit

---
+++ Loop33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Loop22
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a, b, c;
+            for (a = 1; a <= 7; a++)
+            {
+                for (b = 1; b <= a; b++)
+                {
+                    Console.Write(a + "");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
